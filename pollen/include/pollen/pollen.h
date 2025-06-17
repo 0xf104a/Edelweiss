1012640ec9e4ba8ad635534a639584de4e04f1ae
f104a <f104a@f104a.io>
2025-06-17 18:21:08 +0200

    WIP: fork bpf adaptation for AOSP

--- pollen/include/pollen/pollen.h
+++ pollen/include/pollen/pollen.h
@@ -1,6 +1,10 @@
 #ifndef POLLEN_H
 #define POLLEN_H
 
-#define POLLEN_TRACE_TAG "pollen"
+#define POLLEN_TRACE_TAG "pollen" /* common tag for bpf_printk */
+
+#ifndef SEC /* if current environment does not has SEC yet */
+#define SEC(NAME) __attribute__((section(NAME), used)) /* define our own SEC macro */
+#endif
 
 #endif //POLLEN_H
--- pollen/src/forkMonitor.c
+++ pollen/src/forkMonitor.c
@@ -1,7 +1,12 @@
 #include <linux/bpf.h>
+
+#ifdef ANDROID
+#include <bpf_helpers.h>
+#else
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
+#endif
 
 #include <pollen/pollen.h>
 #include <pollen/fork.h>

f6ad003d2d96009e7782d318e543b44c873723c7
f104a <f104a@f104a.io>
2025-07-08 21:59:33 +0200

    Make also compilation on x86_64 Linux possible

--- pollen/src/netMonitor.c
+++ pollen/src/netMonitor.c
@@ -1,11 +1,11 @@
 #ifdef ANDROID
-// Usually this is done by uniquitus bpf_helpers.h but on Android CO-RE programs can not use it
 #include <bpf_helpers.h>
 #else
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_tracing.h>
+#include <vmlinux.h> /* Not real CO-RE, but might be useful to extract structures for the current kernel */
 #endif
 
 #include <pollen/pollen.h>
